f3bd749fdd1c800ef19737f63d6782e8d594ab87
Bruce Xiong <xiongty@outlook.com>
2020-06-16 22:04:02 +0800

    MiniProgram.AppCode.getUnlimited

--- test/unit/mini-program/app-code.ts
+++ test/unit/mini-program/app-code.ts
@@ -6,4 +6,20 @@ describe("MiniProgram.AppCode", function() {
     const result = await mini.appCode.createQRCode("page/index/index", 280);
     expect(result.byteLength).gt(3000);
   });
+
+  it.skip("get", async function() {
+    const result = await mini.appCode.get({
+      path: "page/index/index",
+      width: 280
+    });
+    expect(result.byteLength).gt(3000);
+  });
+
+  it("getUnlimited", async function() {
+    const result = await mini.appCode.getUnlimited({
+      scene: "test",
+      width: 280
+    });
+    expect(result.byteLength).gt(3000);
+  });
 });
